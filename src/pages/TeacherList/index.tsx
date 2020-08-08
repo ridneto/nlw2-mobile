6740545cf4bb25f11050ad3eae08f95c0fb20c95
ridneto <ridineu@hotmail.com>
2020-08-08 17:22:31 -0300

    Component header

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import PageHeader from '../../components/PageHeader';
 
 import styles from './styles'
 
 const TeacherList: React.FC = () => {
     return (
         <View style={styles.container}>
+            <PageHeader title="Proffys Diponíveis"></PageHeader>
             <Text> teacher list </Text>
         </ View>
     )
